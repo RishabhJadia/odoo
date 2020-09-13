0bbeff5bda372cf3087182faf9b5e2afa562c533
Rishabh Jadia (rja-odoo) <26570284+RishabhJadia@users.noreply.github.com>
2020-09-13 14:54:59 +0530

    Update section_and_note_fields_backend.js

--- addons/account/static/src/js/section_and_note_fields_backend.js
+++ addons/account/static/src/js/section_and_note_fields_backend.js
@@ -218,8 +218,8 @@ var SectionAndNoteFieldText = function (parent, name, record, options) {
 fieldRegistry.add('section_and_note_one2many', SectionAndNoteFieldOne2Many);
 fieldRegistry.add('section_and_note_text', SectionAndNoteFieldText);
 return {
-    'SectionAndNoteFieldOne2Many':SectionAndNoteFieldOne2Many,
-    'SectionAndNoteFieldOne2Many':SectionAndNoteFieldOne2Many,
-    'SectionAndNoteFieldText':SectionAndNoteFieldText
+    SectionAndNoteFieldOne2Many:SectionAndNoteFieldOne2Many,
+    SectionAndNoteFieldOne2Many:SectionAndNoteFieldOne2Many,
+    SectionAndNoteFieldText:SectionAndNoteFieldText
 }
 });
